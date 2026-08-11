3df35cfc5a33f46361651c643409944442b0be19
zyt11123 <windowsystem404@163.com>
2026-08-11 10:18:39 +0800

    Fix: make vendor optional, add TARGET_NO_KERNEL, set ALLOW_MISSING_DEPENDENCIES before lunch

--- device/realme_gt_neo/omni_realme_gt_neo.mk
+++ device/realme_gt_neo/omni_realme_gt_neo.mk
@@ -12,7 +12,7 @@ PRODUCT_MODEL := RMX3031
 PRODUCT_MANUFACTURER := realme
 
 # Inherit from common TWRP configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
 
 # Device-specific properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -38,38 +38,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
-
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.mt6893
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.mt6893
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.mt6893
-
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk_package
-
-# Call the proprietary setup
-$(call inherit-product, vendor/realme/realme_gt_neo/realme_gt_neo-vendor.mk)
+# Call the proprietary setup (optional)
+$(call inherit-product-if-exists, vendor/realme/realme_gt_neo/realme_gt_neo-vendor.mk)
